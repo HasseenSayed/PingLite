d9890ea12db210ae112c074fbdca55eb0d3f7f43
Hasseen Sayed <hasseen012@gmail.com>
2024-09-12 20:24:45 +0200

    retarded endpoint works

--- api/api.py
+++ api/api.py
@@ -1,20 +1,26 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, jsonify
 import user
 
 app = Flask(__name__)
 
-conn = sqlite3.connect("C:\\Users\\Hasseen Sayed\\Desktop\\PingLite\\database.db")
-cur = conn.cursor()
-query = "INSERT INTO users (username, first_name, last_name, password) VALUES ('Tester', 'John', 'Doe', 'password123');"
-res = cur.execute(query)
-conn.commit()
 
 
 @app.route("/register", methods=["POST"])
 def register_user():
-    pass
+    conn = sqlite3.connect("C:\\Users\\Hasseen Sayed\\Desktop\\PingLite\\database.db")
+    cur = conn.cursor()
+    query = "INSERT INTO users (username, first_name, last_name, password) VALUES ('Tester', 'John', 'Doe', 'password123');"
+    res = cur.execute(query)
+    conn.commit()
 
-@app.route("/getuser")
-def get_user():
-    pass
+
+@app.route("/getuser/<username>")
+def get_user(username):
+    with sqlite3.connect("C:\\Users\\Hasseen Sayed\\Desktop\\PingLite\\database.db") as conn:
+        cur = conn.cursor()
+        query = "SELECT username, first_name, last_name, password FROM users where username like ?"
+        res = cur.execute(query, (username, ))
+        row = res.fetchone()
+        usr = user.User(row[0], row[1], row[2], row[3])
+        return usr.toJSON()
