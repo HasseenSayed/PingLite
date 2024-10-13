53622d587965a06eebd332bfa053982de95063b2
Hasseen Sayed <hasseen012@gmail.com>
2024-10-14 00:20:29 +0200

    proxy things

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, redirect, request
+from werkzeug.middleware.proxy_fix import ProxyFix
 import requests
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+)
 
 @app.route("/")
 def practice():
